e7490063271d1f20757d46d1c6d581425dd2c901
Jean Figeuroa <jbster559@gmail.com>
2025-05-09 08:59:17 -0700

    Added recipe and ingrediant models

---
+++ COMP003B.SP25.FinalProject.FigueroaJ/Models/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003B.SP25.FinalProject.FigueroaJ.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }    
+        public string Name { get; set; }  
+        public double Quantity { get; set; }
+        public string Unit { get; set; }         
+        public int RecipeId { get; set; }              
+    }
+}
---
+++ COMP003B.SP25.FinalProject.FigueroaJ/Models/Recipe.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003B.SP25.FinalProject.FigueroaJ.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string RecipeTitle { get; set; }
+        public string RecipeDescription { get; set; }
+        public string RecipeInstructions { get; set; }
+        public string RecipeType { get; set; } //Breakfast, lunch, dinner, possibly with diet restrictions such as vegan or keto (Might keep that in the ingredeiant model)
+    }
+}
