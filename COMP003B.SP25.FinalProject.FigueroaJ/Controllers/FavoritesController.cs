38c50473b28d78044847354ebee55ac6ebd6928d
Jean Figeuroa <jbster559@gmail.com>
2025-05-09 15:51:17 -0700

    Created Latter 3 VCs

---
+++ COMP003B.SP25.FinalProject.FigueroaJ/Controllers/FavoritesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP25.FinalProject.FigueroaJ.Models;
+
+namespace COMP003B.SP25.FinalProject.FigueroaJ.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Favorites
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Favorites.Include(f => f.Recipe).Include(f => f.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Favorites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorites
+                .Include(f => f.Recipe)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.FavoriteId == id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        // GET: Favorites/Create
+        public IActionResult Create()
+        {
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Favorites/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FavoriteId,UserId,RecipeId,Note,Visibility")] Favorite favorite)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(favorite);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId", favorite.RecipeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", favorite.UserId);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorites.FindAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId", favorite.RecipeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", favorite.UserId);
+            return View(favorite);
+        }
+
+        // POST: Favorites/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FavoriteId,UserId,RecipeId,Note,Visibility")] Favorite favorite)
+        {
+            if (id != favorite.FavoriteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(favorite);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FavoriteExists(favorite.FavoriteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId", favorite.RecipeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", favorite.UserId);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorites
+                .Include(f => f.Recipe)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.FavoriteId == id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        // POST: Favorites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var favorite = await _context.Favorites.FindAsync(id);
+            if (favorite != null)
+            {
+                _context.Favorites.Remove(favorite);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FavoriteExists(int id)
+        {
+            return _context.Favorites.Any(e => e.FavoriteId == id);
+        }
+    }
+}
---
+++ COMP003B.SP25.FinalProject.FigueroaJ/Controllers/IngredientsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP25.FinalProject.FigueroaJ.Models;
+
+namespace COMP003B.SP25.FinalProject.FigueroaJ.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IngredientsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ingredients
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Ingredients.Include(i => i.Recipe);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Ingredients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients
+                .Include(i => i.Recipe)
+                .FirstOrDefaultAsync(m => m.IngredientId == id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Create
+        public IActionResult Create()
+        {
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId");
+            return View();
+        }
+
+        // POST: Ingredients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IngredientId,Name,Quantity,Diet,RecipeId")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ingredient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId", ingredient.RecipeId);
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId", ingredient.RecipeId);
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IngredientId,Name,Quantity,Diet,RecipeId")] Ingredient ingredient)
+        {
+            if (id != ingredient.IngredientId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ingredient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IngredientExists(ingredient.IngredientId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "RecipeId", "RecipeId", ingredient.RecipeId);
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients
+                .Include(i => i.Recipe)
+                .FirstOrDefaultAsync(m => m.IngredientId == id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient != null)
+            {
+                _context.Ingredients.Remove(ingredient);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IngredientExists(int id)
+        {
+            return _context.Ingredients.Any(e => e.IngredientId == id);
+        }
+    }
+}
