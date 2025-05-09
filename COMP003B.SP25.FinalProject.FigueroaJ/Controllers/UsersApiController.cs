70df98a779b1f852a6a621f9714e2e0168c08db8
Jean Figeuroa <jbster559@gmail.com>
2025-05-09 16:20:09 -0700

    WebApi created! Modeled off the API from the lecture activity

---
+++ COMP003B.SP25.FinalProject.FigueroaJ/Controllers/UsersApiController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.SP25.FinalProject.FigueroaJ.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP25.FinalProject.FigueroaJ.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        // GET
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, user);
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User updatedUser)
+        {
+            if (id != updatedUser.UserId)
+                return BadRequest();
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return NotFound();
+
+            user.Username = updatedUser.Username;
+            user.Email = updatedUser.Email;
+            user.Password = updatedUser.Password;
+            user.Bio = updatedUser.Bio;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return NotFound();
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- COMP003B.SP25.FinalProject.FigueroaJ/Models/User.cs
+++ COMP003B.SP25.FinalProject.FigueroaJ/Models/User.cs
@@ -8,7 +8,7 @@ namespace COMP003B.SP25.FinalProject.FigueroaJ.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; } //passwords should be hased but thats not something i know how to do
-        public string Bio { get; set; }
+        public string? Bio { get; set; }
 
         public ICollection<Favorite> Favorites { get; set; } // User can have many Favorites
         public ICollection<Review> Reviews { get; set; } // User can have many Reviews
