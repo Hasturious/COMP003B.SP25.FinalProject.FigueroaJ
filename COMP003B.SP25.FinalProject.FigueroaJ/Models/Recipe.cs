44c5a7e1d39d099e67eec60329cbbd3607ce67ca
Jean Figeuroa <jbster559@gmail.com>
2025-05-09 15:39:36 -0700

    Added new attirbute for migration purposes

--- COMP003B.SP25.FinalProject.FigueroaJ/Models/Recipe.cs
+++ COMP003B.SP25.FinalProject.FigueroaJ/Models/Recipe.cs
@@ -11,6 +11,8 @@ namespace COMP003B.SP25.FinalProject.FigueroaJ.Models
         public string RecipeInstructions { get; set; }
         public string RecipeType { get; set; }
 
+        public string? Notes { get; set; } // I added this for the migration requirement, will likely go unused
+
         public ICollection<Ingredient> Ingredients { get; set; } //Recipe has many ingrediants
         public ICollection<Favorite> Favorites { get; set; } // Recipe can be favorited by many users
         public ICollection<Review> Reviews { get; set; } // Recipe can have many reviews
