88bd0f6c8a70f5de107488d7b28a741d8e8cadd7
Jean Figeuroa <jbster559@gmail.com>
2025-05-09 17:06:18 -0700

    More validation issue

--- COMP003B.SP25.FinalProject.FigueroaJ/Models/Recipe.cs
+++ COMP003B.SP25.FinalProject.FigueroaJ/Models/Recipe.cs
@@ -6,6 +6,7 @@ namespace COMP003B.SP25.FinalProject.FigueroaJ.Models
     public class Recipe
     {
         public int RecipeId { get; set; }
+        //public int UserId { get; set; }
         public string RecipeTitle { get; set; }
         public string RecipeDescription { get; set; }
         public string RecipeInstructions { get; set; }
